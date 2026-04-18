e865c1c236525eb44a2bf259ae54b33a25ad3ad7
Susmitha <susmitha.dev48@gmail.com>
2026-04-18 15:03:19 -0400

    1C Repository Layer

---
+++ cart-service/src/main/java/com/example/cart_service/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.cart_service.repository;
+
+import com.example.cart_service.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
+
+}
